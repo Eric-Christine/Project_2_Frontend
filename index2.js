7034732cab081c5eef3d72d15889d0234afc8253
Eric Chrsitine <ericjchristine@gmail.com>
2020-10-22 19:22:25 -0400

    added time stamp

--- index2.js
+++ index2.js
@@ -13,19 +13,24 @@ function showSearchResult(data){
     const confirmed_cases = result.map(object => object.confirmed)
     const confirmed_deaths = result.map(object => object.deaths)
     const confirmed_cases_result = confirmed_cases[0]
+    const lastUpdated = result.map(object => object.lastUpdate)
     console.log(confirmed_cases_result)
    
 
     // create element
     const displayCases = document.createElement('li')
     const displayDeaths = document.createElement('li')
+    const displayUpdate = document.createElement('li')
     displayCases.innerText = `${search_term}'s confirmed cases: ${confirmed_cases_result}`
     displayDeaths.innerText = `${search_term}'s confirmed deaths: ${confirmed_deaths}`
+    displayUpdate.innerText = `Statisticss last updated: ${lastUpdated}`
 
 
     // append element
-    const bullet_point =  document.querySelector('li')
-    bullet_point.append(displayCases, displayDeaths)
+    const bullet_point =  document.querySelector('ul')
+    bullet_point.append(displayCases, displayDeaths, lastUpdated)
+
+
 
 
 
