7a2fc925b864c8ce29b2cfbb9746aac054ff82b9
Eric Chrsitine <ericjchristine@gmail.com>
2020-10-23 12:22:21 -0400

    more search result data

--- index2.js
+++ index2.js
@@ -4,7 +4,16 @@ console.log(search_term)
 
 fetch('http://localhost:3000/locations')
     .then (response => response.json())
-    .then (result => showSearchResult(result))
+    .then (renderdata)
+
+function renderdata(data){
+    showSearchResult(data)
+    displayDeaths(data)
+}
+
+
+function displayDeaths(data) {
+}
 
 
 
@@ -12,23 +21,32 @@ function showSearchResult(data){
     const result = data.filter(data => data.city == `${search_term}`)
     const confirmed_cases = result.map(object => object.confirmed)
     const confirmed_deaths = result.map(object => object.deaths)
+    const confirmed_recoveries = result.map(object => object.recovered)
+    console.log(confirmed_recoveries)
     const confirmed_cases_result = confirmed_cases[0]
     const lastUpdated = result.map(object => object.lastUpdate)
     console.log(confirmed_cases_result)
    
 
     // create element
+
     const displayCases = document.createElement('li')
     const displayDeaths = document.createElement('li')
+    const displayRecovered = document.createElement('li')
     const displayUpdate = document.createElement('li')
+
+    // modifiy element
     displayCases.innerText = `${search_term}'s confirmed cases: ${confirmed_cases_result}`
     displayDeaths.innerText = `${search_term}'s confirmed deaths: ${confirmed_deaths}`
+    displayRecovered.innerText =`${search_term}'s recovered cases: ${confirmed_recoveries}`
     displayUpdate.innerText = `Statisticss last updated: ${lastUpdated}`
+    
 
 
     // append element
     const bullet_point =  document.querySelector('ul')
-    bullet_point.append(displayCases, displayDeaths, lastUpdated)
+    bullet_point.append(displayCases, displayDeaths, displayRecovered, lastUpdated)
+
 
 
 
